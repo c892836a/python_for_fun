ae4afc468f713035b4d65d18469da505a346edf9
kakeilee <kakeilee@iii.org.tw>
2018-12-21 12:59:27 +0800

    add blank line exception

--- massOpenUrl.py
+++ massOpenUrl.py
@@ -5,11 +5,13 @@ def main():
     cmd = ""
     with open("./list/url_list", "r", encoding='utf8', newline=None) as f:
         for line in f:
+            if line.strip() == "":
+                continue
             if cmd == "":
                 cmd = 'start "" "{}"'.format(line)
             else:
                 cmd += ' & start "" "{}"'.format(line)
-
+    print(cmd)
     os.system("start cmd /c \"{}\"".format(cmd.replace("\n", "")))
 
 
