cb66e028803258dba3294a9c48cbc66ae793e411
kakeilee <kakeilee@iii.org.tw>
2018-12-27 15:54:18 +0800

    update logger class

--- getLogger.py
+++ getLogger.py
@@ -2,18 +2,23 @@ import logging
 
 
 class GetLogger:
-    def __init__(self):
+    user = ""
+    filename = ""
+
+    def __init__(self, user, filename):
+        self.user = user
+        self.filename = filename
         pass
 
-    def initial_logger(self, user, filename):
+    def initial_logger(self):
         logging.basicConfig(level=logging.DEBUG,
                             format='%(asctime)-21s %(name)-6s %(levelname)-10s %(message)s',
                             datefmt='%Y-%m-%d %H:%M:%S',
-                            handlers=[logging.FileHandler('./logs/{}.log'.format(filename), 'w', 'utf-8'), ])
+                            handlers=[logging.FileHandler('./logs/{}.log'.format(self.filename), 'w', 'utf-8'), ])
 
         console = logging.StreamHandler()
         console.setLevel(logging.INFO)
         formatter = logging.Formatter('%(name)s: %(levelname)-10s %(message)s')
         console.setFormatter(formatter)
-        logging.getLogger(user).addHandler(console)
-        return logging.getLogger(user)
+        logging.getLogger(self.user).addHandler(console)
+        return logging.getLogger(self.user)
