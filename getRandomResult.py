d168b26f674a6dc6a6f36623957c1e49c425e36d
kakeilee <kakeilee@iii.org.tw>
2019-04-08 15:48:38 +0800

    update random method

--- getRandomResult.py
+++ getRandomResult.py
@@ -1,18 +1,20 @@
 import random
 import os
+# import time
 
-random_range = 999
-random_number = 1001
-
+RANDOM_RANGE = 3571
+RANDOM_NUMBER = 1001
+# RANDOM_FACTOR = int(str(time.time()).replace(".", ""))
 
 def start_random():
     count = 0
-    for _i in range(random_number):
-        if random.randint(0, random_range) > int(random_range / 2):
+    for _i in range(RANDOM_NUMBER):
+        result = (random.randint(1, RANDOM_RANGE) * random.randint(1, RANDOM_RANGE)) % RANDOM_RANGE
+        if result >= int(RANDOM_RANGE / 2):
             count += 1
 
     print(count)
-    if count < int(random_number / 2) + 1:
+    if count < (int(RANDOM_NUMBER / 2) + 1):
         print("Go to shower")
     else:
         print("Please start your show!")
